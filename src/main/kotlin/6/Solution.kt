2960f24fe292c0f5b5e116d26edf6ae0714cfe6f
waiwaing <waiwai95@gmail.com>
2021-12-06 23:28:08 +0000

    Day 6

--- src/main/kotlin/6/Solution.kt
+++ src/main/kotlin/6/Solution.kt
@@ -1,9 +1,41 @@
 package `6`
 
+val default_map = (0..8).associateWith { 0L }
+
 fun run_a(input: List<String>): String {
-    return ""
+    val initialState = default_map +
+            input.first().split(",").map(String::toInt)
+                .groupBy { it }
+                .mapValues { it.value.count().toLong() }
+
+    val finalState = (1..80).fold(initialState) { state, _ ->
+        state.mapValues {
+            when (it.key) {
+                6 -> state.getOrDefault(7, 0) + state.getOrDefault(0, 0)
+                8 -> state.getOrDefault(0, 0)
+                else -> state.getOrDefault(it.key + 1, 0)
+            }
+        }
+    }
+
+    return finalState.values.sum().toString()
 }
 
 fun run_b(input: List<String>): String {
-    return ""
+    val initialState = default_map +
+            input.first().split(",").map(String::toInt)
+                .groupBy { it }
+                .mapValues { it.value.count().toLong() }
+
+    val finalState = (1..256).fold(initialState) { state, _ ->
+        state.mapValues {
+            when (it.key) {
+                6 -> state.getOrDefault(7, 0) + state.getOrDefault(0, 0)
+                8 -> state.getOrDefault(0, 0)
+                else -> state.getOrDefault(it.key + 1, 0)
+            }
+        }
+    }
+
+    return finalState.values.sum().toString()
 }

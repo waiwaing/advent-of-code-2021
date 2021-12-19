ddeef92bcf456e2c9e33e935106e799f31a3fe85
waiwaing <waiwai95@gmail.com>
2021-12-19 01:04:08 -0800

    Refactor day 17

--- src/main/kotlin/17/Solution.kt
+++ src/main/kotlin/17/Solution.kt
@@ -22,8 +22,7 @@ fun run_a(input: List<String>): String {
                 candidate = XyPair(0, candidate.y + 1)
                 flag = 3000
             }
-            (currentSign == 0) || (currentSign == indicator) ->
-                candidate = XyPair(candidate.x + indicator, candidate.y)
+            (currentSign == 0) || (currentSign == indicator) -> candidate = XyPair(candidate.x + indicator, candidate.y)
             else -> {
                 candidate = XyPair(0, candidate.y + 1)
                 flag -= 1
@@ -32,52 +31,43 @@ fun run_a(input: List<String>): String {
         currentSign = indicator
     }
 
-    println("Highest Y: ${best.y}")
-    return highestYForTrajectory(best).toString()
+    return highestYReached(best).toString()
 }
 
 fun run_b(input: List<String>): String {
     val inputInt = input.map(String::toInt)
     val targetArea = XyPair(inputInt[0]..inputInt[1], inputInt[2]..inputInt[3])
 
-    return (0..1000).cartesianProduct(-1000..250).count {
-        isTrajectoryForTarget(XyPair(it.first, it.second), targetArea) == 0
-    }.toString()
+    return (0..1000).cartesianProduct(-1000..250)
+        .count { isTrajectoryForTarget(XyPair(it.first, it.second), targetArea) == 0 }.toString()
+}
+
+private fun calculateTrajectory(
+    trajectory: XyPair<Int>, shouldStop: (position: XyPair<Int>, velocity: XyPair<Int>) -> Boolean
+): XyPair<Int> {
+    var position = XyPair(0, 0)
+    var velocity = trajectory
+
+    while (!shouldStop(position, velocity)) {
+        position = XyPair(position.x + velocity.x, position.y + velocity.y)
+        velocity = XyPair(velocity.x - velocity.x.sign, velocity.y - 1)
+    }
+
+    return position
 }
 
 private fun isTrajectoryForTarget(trajectory: XyPair<Int>, target: XyPair<IntRange>): Int {
-    var position = XyPair(0, 0)
-    var velocity = trajectory
+    val position = calculateTrajectory(trajectory) { position, velocity ->
+        (position.x in target.x && position.y in target.y) || // in target
+                (position.y < target.y.first && velocity.y < 0 && position.x < target.x.last) || // dropped below target
+                (position.x > target.x.last) // overshoot
+    }
 
-    while (true) {
-        position = XyPair(position.x + velocity.x, position.y + velocity.y)
-        velocity = XyPair(stepTowards0(velocity.x), velocity.y - 1)
-
-        when {
-            position.x in target.x && position.y in target.y -> return 0
-            position.y < target.y.first && velocity.y < 0 && position.x < target.x.last -> return 1
-            position.x > target.x.last -> return -1
-        }
+    return when {
+        position.x in target.x && position.y in target.y -> 0
+        position.x > target.x.last -> -1
+        else -> 1
     }
 }
 
-private fun highestYForTrajectory(trajectory: XyPair<Int>): Int {
-    var position = XyPair(0, 0)
-    var velocity = trajectory
-
-    while (true) {
-        if (velocity.y < 0) {
-            return position.y
-        }
-
-        position = XyPair(position.x + velocity.x, position.y + velocity.y)
-        velocity = XyPair(stepTowards0(velocity.x), velocity.y - 1)
-    }
-}
-
-private fun stepTowards0(value: Int) = when (value.sign) {
-    0 -> 0
-    1 -> value - 1
-    -1 -> value + 1
-    else -> throw Exception("wtf")
-}
+private fun highestYReached(trajectory: XyPair<Int>) = calculateTrajectory(trajectory) { _, vel -> vel.y < 0 }.y
